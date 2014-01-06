954cfc5efd63890db037a42d659ab07bddecda9e
gssiyankai <syk.greg@gmail.com>
2014-01-06 20:03:21 +0100

    problem 2

--- h99_p01_10.hs
+++ h99_p01_10.hs
@@ -1,6 +1,11 @@
-module H99( myLast ) where
+module H99( myLast, myButLast ) where
 
 -- Problem 1
 myLast :: [a] -> a
 myLast [x] = x
-myLast (x : xs) = myLast xs
+myLast (_ : xs) = myLast xs
+
+-- Problem 2
+--myButLast :: [a] -> a
+myButLast [x, _] = x
+myButLast (_ : xs) = myButLast xs
--- h99_p01_10_test.hs
+++ h99_p01_10_test.hs
@@ -1,4 +1,4 @@
-import H99( myLast )
+import H99( myLast, myButLast )
 import Test.HUnit
 
 problem1 = TestCase $ do
@@ -7,7 +7,14 @@ problem1 = TestCase $ do
 	assertEqual 
   		"Find the last element of a list of chars" 'z' (myLast ['x','y','z'])
 
+problem2 = TestCase $ do
+	assertEqual 
+  		"Find the last but one element of a list of integers" 3 (myButLast [1,2,3,4])
+	assertEqual 
+  		"Find the last but one element of a list of chars" 'y' (myButLast ['a'..'z'])
+
 main = runTestTT $ TestList
 			[
-				problem1 
+				problem1,
+				problem2
 			]
